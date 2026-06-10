f790f44df816486f14592f4e3a678f593ff7756b
LuisEMarc <lemarc95@hotmail.com>
2026-06-10 12:51:02 -0600

    Cambio general Se consume toda la API de Gatitos Se muestra imagen generica para los gatitos sin imagen Se Agregó la paginación para no sobrecargar la vista

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,7 +1,7 @@
 function getCatImage(cat) {
   return cat.reference_image_id
     ? `https://cdn2.thecatapi.com/images/${cat.reference_image_id}.jpg`
-    : "https://placehold.co/400x400?text=No+Image";
+    : "assets/images/404-gatito.jpg";
 }
 
 function createCatCard(cat, index) {
@@ -9,9 +9,9 @@ function createCatCard(cat, index) {
     <div class="cat-card" data-index="${index}">
 
       <img
-        src="${getCatImage(cat)}"
-        alt="${cat.name}"
-      >
+      src="${getCatImage(cat)}"
+      alt="${cat.name}"
+      onerror="this.src='assets/images/404-gatito.jpg'">
 
       <div class="cat-info">
 
@@ -25,10 +25,10 @@ function createCatCard(cat, index) {
   `;
 }
 
-function renderCats(cats) {
+function renderCats(cats, startIndex = 0) {
   const grid = document.getElementById("catsGrid");
 
   grid.innerHTML = cats
-    .map(createCatCard)
+    .map((cat, index) => createCatCard(cat, startIndex + index))
     .join("");
 }
